ab51e51c8def08753f41cdcdb279b6fa6c3894af
kvanschuijlenburg <kvanschuijlenburg@gmail.com>
2022-05-04 15:59:52 +0200

    baseline file

---
+++ baseline.py
@@ -0,0 +1,10 @@
+from Dataset import Dataset
+
+def main():
+    trainingSet = Dataset.trainingSet()
+    testingSet = Dataset.testingSet()
+    # create linear model, and fit it on the training set
+    # test the model on the testing set
+
+if __name__ == '__main__':
+    main()
