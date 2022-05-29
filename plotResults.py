3a68f0dd6f69f51ac7833cf964a2514e59885d58
kvanschuijlenburg <kvanschuijlenburg@gmail.com>
2022-05-29 22:55:45 +0200

    plot results

--- plotResults.py
+++ plotResults.py
@@ -1,25 +1,29 @@
 import os
-import csv
 import matplotlib.pyplot as plt
 import pandas as pd
 
+twoColumnFigureWidth = 20 # For latex
+
+
+pltTrainAcc = plt.figure(figsize = (twoColumnFigureWidth, 10))
+pltValAcc = plt.figure(figsize = (twoColumnFigureWidth, 10))
 
 for filename in os.listdir("./gridsearchResults"):
+    parameters = dict([parameter.split('=') for parameter in filename.removesuffix('.csv').split('_')])
+
     dataframe = pd.read_csv(os.path.join("gridsearchResults", filename))
-    dataframe.plot(x="Epoch", y=["accuracy","validation accuracy"])
-    plt.show()
+    #dataframe.plot(x="Epoch", y=["accuracy","validation accuracy"])
+    #plot = dataframe.plot(x="Epoch", y=["validation accuracy"])
 
-    with open(os.path.join("gridsearchResults", filename), newline='') as file:
-        rows = csv.reader(file, delimiter=',')#, quotechar='|')
-        trainingLoss = []
-        trainingAccuracy = []
-        validationLoss = []
-        validationAccuracy = []
-        
-        for index, row in enumerate(rows):
-            if index >0:
-                trainingLoss.append(row[1])
-                trainingAccuracy.append(row[2])
-                validationLoss.append(row[3])
-                validationAccuracy.append(row[4])
+    axTrainAcc = pltTrainAcc.add_subplot() 
+    axTrainAcc.plot(dataframe["Epoch"], dataframe["accuracy"])
+    
+    axValAcc = pltValAcc.add_subplot() 
+    axValAcc.plot(dataframe["Epoch"], dataframe["validation accuracy"])
 
+
+pltTrainAcc.savefig("./figures/crossValidateTestAccuracy.png", dpi = 300, bbox_inches='tight')
+pltValAcc.savefig("./figures/crossValidateValidationAccuracy.png", dpi = 300, bbox_inches='tight')
+plt.close()
+plt.cla()
+plt.clf() 
